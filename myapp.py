354aab5e875229d4bea2eb481c97b6b5e13bc073
vineel <147791244+vineeldhanani@users.noreply.github.com>
2024-01-08 12:29:04 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,9 +5,9 @@ st.header("Types of Cats")
 col1, col2 = st.columns(2)
 with col1:
   st.subheader("Persian Cat")
-  st.image("./Persian.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.image("./image.jpg", caption="Persian Cat", width=300,use_column_width=True)
   st.write("Persian cats are cute")
 with col2:
   st.subheader("Ragdoll Cat")
-  st.image("./ragdoll.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.image("./8.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
   st.write("Ragdoll cats are proud")
