cb5c836ece64fc3d7b53d10ecd29bec6edf597ac
vineel <147791244+vineeldhanani@users.noreply.github.com>
2024-01-08 12:27:17 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,1 +1,13 @@
+import streamlit as st
+st.set_page_config(page_title='Cats')
+st.header("Types of Cats")
 
+col1, col2 = st.columns(2)
+with col1:
+  st.subheader("Persian Cat")
+  st.image("./Persian.jpg", caption="Persian Cat", width=300,use_column_width=True)
+  st.write("Persian cats are cute")
+with col2:
+  st.subheader("Ragdoll Cat")
+  st.image("./ragdoll.jpg", caption="Ragdoll Cat", width=300,use_column_width=True)
+  st.write("Ragdoll cats are proud")
